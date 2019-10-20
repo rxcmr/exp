74c26138e5868905c850527cecdde84e75bcab86
rxcmr <lythe1107@icloud.com>
2019-10-20 21:08:37 +0800

    the failures of the midpoint and trapezoidal implementation are removed :(

--- src/main/java/expr/EX8.java
+++ src/main/java/expr/EX8.java
@@ -1,32 +1,25 @@
 package expr;
 
+import org.jetbrains.annotations.Contract;
+
 interface Function { double evaluate(double x); }
 
 interface Integral { double integrate(double lowerBounds, double upperBounds); }
 
-class _EX8_Midpoint_Rule {
-   Function f_of_x; // the function f(x)
-   Integral integral = (a, b) -> (b - a) * f_of_x.evaluate((a + b) / 2);
-}
-
-class _EX8_Trapezoidal_Rule {
-   Function f_of_x; // the function f(x)
-   Integral integral = (a, b) -> (b - a) * ((f_of_x.evaluate(a) + f_of_x.evaluate(b)) / 2);
+class _EX8_Return_Limits {
+   private double c;
+   @Contract(pure = true)
+   _EX8_Return_Limits(double c) { this.c = c; }
+   Function f_of_x;
+   Integral integral = (a, b) -> f_of_x.evaluate(c) * b - f_of_x.evaluate(c) * a;
 }
 
 public class EX8 {
    public static void main(String[] args) {
-      _EX8_Midpoint_Rule midpointRule = new _EX8_Midpoint_Rule();
-      midpointRule.f_of_x = x -> 2 * x; // evaluate the function f(x) = 2x
-      midpointRule.f_of_x.evaluate(9); // f(9) = 2 * 9, f(9) = 81
-      double mpr_result = midpointRule.integral.integrate(0, 9); // Integral[0, 9] 2x dx
-      System.out.println("Midpoint rule result: " + mpr_result);
-
-      _EX8_Trapezoidal_Rule trapezoidalRule = new _EX8_Trapezoidal_Rule();
-      trapezoidalRule.f_of_x = x -> 2 * x; // evaluate the function f(x) = 2x
-      trapezoidalRule.f_of_x.evaluate(9); // f(9) = 2 * 9, f(9) = 81
-      double tzr_result = trapezoidalRule.integral.integrate(0, 9); // Integral[0, 9] 2x dx
-      System.out.println("Trapezoidal rule result: " + tzr_result);
+      _EX8_Return_Limits returnLimits = new _EX8_Return_Limits(10);
+      returnLimits.f_of_x = x -> 2 * x;
+      double rlm_result = returnLimits.integral.integrate(0, 5);
+      System.out.println("Return limits result: " + rlm_result);
    }
 }
 
