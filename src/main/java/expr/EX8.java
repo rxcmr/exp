0d8acc59f64cde1b398ac952b425b85186d29ca3
rxcmr <lythe1107@icloud.com>
2019-10-20 18:27:19 +0800

    integrals! lambda expressions! multiple interfaces! multiple classes!

---
+++ src/main/java/expr/EX8.java
@@ -0,0 +1,37 @@
+package expr;
+
+interface Integral {
+   double integrate(double lowerBounds, double upperBounds);
+}
+
+interface Function {
+   double evaluate(double x);
+}
+
+class _EX8_Midpoint_Rule {
+   Function f_of_x; // evaluate the function f(x)
+   Integral integral = (a, b) -> (b - a) * f_of_x.evaluate((a + b) / 2);
+}
+
+class _EX8_Trapezoidal_Rule {
+   Function f_of_x;
+   Integral integral = (a, b) -> (b - a) * ((f_of_x.evaluate(a) + f_of_x.evaluate(b)) / 2);
+}
+
+public class EX8 {
+   public static void main(String[] args) {
+      _EX8_Midpoint_Rule midpointRule = new _EX8_Midpoint_Rule();
+      midpointRule.f_of_x = x -> 2 * x; // evaluate the function f(x) = 2x
+      midpointRule.f_of_x.evaluate(9); // f(9) = 2 * 9, f(9) = 81
+      double mpr_result = midpointRule.integral.integrate(0, 9); // Integral[0, 9] 2x dx
+      System.out.println("Midpoint rule result: " + mpr_result);
+
+      _EX8_Trapezoidal_Rule trapezoidalRule = new _EX8_Trapezoidal_Rule();
+      trapezoidalRule.f_of_x = x -> 2 * x; // evaluate the function f(x) = 2x
+      trapezoidalRule.f_of_x.evaluate(9); // f(9) = 2 * 9, f(9) = 81
+      double tzr_result = trapezoidalRule.integral.integrate(0, 9); // Integral[0, 9] 2x dx
+      System.out.println("Trapezoidal rule result: " + tzr_result);
+   }
+}
+
+
