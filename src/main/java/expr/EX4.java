585e6bfcbe13b0b2848358ffdcfacf4bb14822a1
rxcmr <lythe1107@icloud.com>
2019-10-26 19:46:47 +0800

    change in semantics

--- src/main/java/expr/EX4.java
+++ src/main/java/expr/EX4.java
@@ -22,13 +22,17 @@ class EX4 {
          rdr.readLine();
          while ((field = rdr.readLine()) != null) {
             String[] data = field.split(delimiter);
-            hashMap.put(data[0].replace("\"", ""), data[1].replace("\"", ""));
+            data[0] = data[0].replace("\"", "");
+            data[1] = data[1].replace("\"", "");
+            hashMap.put(data[0], data[1]);
          }
       } catch (IOException e) {
          System.err.println("CSV file not found.");
          System.out.println("Creating CSV file...");
          try { createCSV(); }
-         catch (IOException ignored) {}
+         catch (IOException ex) {
+            System.err.println("Unable to create CSV file.");
+         }
       } finally { passwordCheck(); }
    }
    
@@ -36,17 +40,15 @@ class EX4 {
       if (hashMap.get(username) == null) {
          isPasswordCorrect = false;
          System.err.println("Parsing failed.");
-      } else {
-         isPasswordCorrect = hashMap.get(username).equals(password);
-      }
+      } else isPasswordCorrect = hashMap.get(username).equals(password);
    }
 
    private void createCSV() throws IOException {
       File file = new File(path);
+      BufferedWriter wtr = new BufferedWriter(new FileWriter(file));
       if (file.exists()) {
          System.err.println("File exists.");
          System.err.println("Overwriting contents...");
-         BufferedWriter wtr = new BufferedWriter(new FileWriter(file));
          wtr.write("\"Username\",\"Password\"");
          // replace these as you please
          wtr.write("\n\"Alan\",\"IaMaLaN1234\"");
@@ -54,7 +56,6 @@ class EX4 {
          wtr.write("\n\"Gilbert\",\"1234567890\"");
          wtr.close();
       } else if (file.createNewFile()) {
-         BufferedWriter wtr = new BufferedWriter(new FileWriter(file));
          wtr.write("\"Username\",\"Password\"");
          // replace these as you please
          wtr.write("\n\"Alan\",\"IaMaLaN1234\"");
